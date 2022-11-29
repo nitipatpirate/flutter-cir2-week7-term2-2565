4801c561a6e8161891a66e5dc4537dee310581b8
KLAIIIII <guyyg03546@gmail.com>
2022-11-29 11:48:46 +0700

    update

--- lib/list_View_menu.dart
+++ lib/list_View_menu.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
+class ListMenu {
+  final String? title;
+  final String? subtitle;
+  final IconData? icon;
+  const ListMenu({this.title, this.subtitle, this.icon});
+}
+
+List<ListMenu> menus = [
+  const ListMenu(title: 'MENU-1', subtitle: 'SUB-1', icon: Icons.person),
+  const ListMenu(title: 'MENU-2', subtitle: 'SUB-2', icon: Icons.person_add),
+];
+
 class ListViewMenuPage extends StatelessWidget {
   const ListViewMenuPage({Key? key}) : super(key: key);
 
@@ -8,24 +20,23 @@ class ListViewMenuPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title:Text("List View Menu"),
+        title: Text("List View Menu"),
       ),
       body: Container(
         child: ListView.separated(
-          itemCount: 4,
-          separatorBuilder: (_,index){
+          itemCount: menus.length,
+          separatorBuilder: (_, index) {
             return Divider(color: Colors.red);
           },
-          itemBuilder: (context,index){
+          itemBuilder: (context, index) {
             return ListTile(
-              title: Text('menu-1'),
-              subtitle: Text('sub-1'),
-              leading: CircleAvatar(child: Icon(Icons.person)),
-              trailing: Icon(Icons.arrow_right),
-              onTap:(){
-                debugPrint('menu-1');
-              }
-            );
+                title: Text('${menus[index].title}'),
+                subtitle: Text('${menus[index].subtitle}'),
+                leading: CircleAvatar(child: Icon(menus[index].icon)),
+                trailing: Icon(Icons.arrow_right),
+                onTap: () {
+                  debugPrint('${menus[index].title}');
+                });
           },
         ),
       ),
